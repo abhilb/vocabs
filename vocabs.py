be8116dcceec7d4e3130cc1dbb8a2eb3fc0270e5
Abhilash <abhilashbabuj@gmail.com>
2020-06-11 09:45:26 +0200

    handle indexerror and added some more styling.

--- vocabs.py
+++ vocabs.py
@@ -7,17 +7,25 @@ from flask import Flask, render_template, session, request
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 
-@app.route("/", methods=['GET', 'POST'])
-def index():
+def get_next():
     vocab_txt = requests.get("https://raw.githubusercontent.com/abhilb/Notes/master/vocab.json").text
     vocab = json.loads(vocab_txt)
     count = len(vocab)
     idx = random.randint(0, count)
     data = {}
-    data['question'] = list(vocab.keys())[idx]
-    data['answer'] = list(vocab.values())[idx]
-    if request.method == 'POST':
-        return render_template("index.html", data=data)
-    else:
-        return render_template("index.html", data=data)
+    while True:
+        try:
+            data['question'] = list(vocab.keys())[idx]
+            data['answer'] = list(vocab.values())[idx]
+        except IndexError:
+            continue
+        else:
+            break
+    return data
+
+@app.route("/", methods=['GET', 'POST'])
+def index():    
+    return render_template("index.html", data=get_next())
+    
+
 
