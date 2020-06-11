df3e83491701a992a2da4e20bd9ef11de11cf3c8
Abhilash <abhilashbabuj@gmail.com>
2020-06-11 08:42:20 +0200

    Use one button instead of two to show and get the next word.

--- static/main.js
+++ static/main.js
@@ -1,10 +1,12 @@
 
 $(function() { 
     $("#answer").css('visibility', 'hidden');
+    $("#form_btn").hide();
 
     $("#show_btn").click(() => {
-        console.log("show button is clicked...");
-        
+        console.log("show button is clicked...");        
         $("#answer").css('visibility', 'visible');
+        $("#show_btn").hide();
+        $("#form_btn").show();
     });
 })
