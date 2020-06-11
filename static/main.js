9d0692e98e1531cb1c655520ab3bbafcd4247b2c
Abhilash <abhilashbabuj@gmail.com>
2020-06-12 01:48:24 +0200

    flipping cards

--- static/main.js
+++ static/main.js
@@ -1,12 +1,15 @@
+const zeroAngle = "rotateY(0deg)";
+const flipAngle = "rotateY(180deg)";
+var angle = zeroAngle;
 
-$(function() { 
-    $("#answer").css('visibility', 'hidden');
-    $("#form_btn").hide();
-
-    $("#show_btn").click(() => {
-        console.log("show button is clicked...");        
-        $("#answer").css('visibility', 'visible');
-        $("#show_btn").hide();
-        $("#form_btn").show();
-    });
-})
+$(function() {
+  $(".flip-card").click(() => {
+    console.log("clicked");
+    if(angle === zeroAngle) {
+      $(".flip-card-inner").css("transform", flipAngle);
+      angle = flipAngle;
+    } else {
+      $(".flip-card-inner").css("transform", zeroAngle);         angle = zeroAngle;
+    }
+  });
+});
